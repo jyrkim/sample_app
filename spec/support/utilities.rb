b66e8aeda41368c7ff2ded61bf2002d0f4ed04e4
jyrkim <jyrki.mizaras@gmail.com>
2013-12-01 19:20:24 +0200

    rspec utilities changes

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -12,17 +12,19 @@ include ApplicationHelper
 #end
 
 def sign_in(user)
-	#puts signin_path
-	visit  signin_path  #'/sessions/signin'
-	fill_in "Email", with: user.email.upcase
+	#puts signin_path #'/sessions/signin'
+	visit  signin_path  
+	fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Sign in"
     # Sign in when not using Capybara as well
     cookies[:remember_token] = user.remember_token
 end
 
-RSpec::Matchers::define :have_title do |text|
-  match do |page|
-    Capybara.string(page.body).has_selector?('title', text: text)
-  end
-end
+
+
+#RSpec::Matchers::define :have_title do |text|
+#  match do |page|
+#    Capybara.string(page.body).has_selector?('title', text: text)
+#  end
+#end
